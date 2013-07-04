4e17aabe3f7ebf23ce0781eb769658f169cd5b16
lubson <l.hruban@gmail.com>
2013-07-05 00:39:18 +0200

    Simple inser and query implemented in CodeFirstWithNewDatabase project

--- StartingWithEntityFramework/CodeFirstWithNewDatabase/Program.cs
+++ StartingWithEntityFramework/CodeFirstWithNewDatabase/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.Entity;
 
 namespace CodeFirstWithNewDatabase
 {
@@ -10,6 +11,55 @@ namespace CodeFirstWithNewDatabase
     {
         static void Main(string[] args)
         {
+            using (var db = new BloggingContext())
+            {
+                //Create and save a new Blog
+                Console.Write("Enter a name for a new blog:");
+                var name = Console.ReadLine();
+
+                var blog = new Blog { Name = name };
+                db.Blogs.Add(blog);
+                db.SaveChanges();
+
+                //Display all Blogs in the database
+                var query = from b in db.Blogs
+                            orderby b.Name
+                            select b;
+
+                Console.WriteLine("All blogs in database:");
+                foreach (var record in query)
+                {
+                    Console.WriteLine(record.Name);
+                }
+
+                Console.WriteLine("Press any key to exit...");
+                Console.ReadKey();
+            }
         }
     }
+}
+
+//Business classes
+public class Blog
+{
+    public int BlogId { get; set; }
+    public string Name { get; set; }
+
+    public virtual List<Post> Posts { get; set; }
+}
+
+public class Post
+{
+    public int PostId { get; set; }
+    public string Title { get; set; }
+    public string Content { get; set; }
+
+    public int BlogId { get; set; }
+    public virtual Blog Blog { get; set; }
+}
+
+public class BloggingContext : DbContext
+{
+    public DbSet<Blog> Blogs { get; set; }
+    public DbSet<Post> Posts { get; set; }
 }
