ba9ee2f6dbbe850059069ad3b3514b40395cf5d0
lubson <l.hruban@gmail.com>
2013-07-05 12:15:07 +0200

    Added url to blog in CodeFirstWithNewDatabase project

---
+++ StartingWithEntityFramework/CodeFirstWithNewDatabase/Migrations/201307042155058_InitialCreate.cs
@@ -0,0 +1,42 @@
+namespace CodeFirstWithNewDatabase.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialCreate : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Blogs",
+                c => new
+                    {
+                        BlogId = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                    })
+                .PrimaryKey(t => t.BlogId);
+            
+            CreateTable(
+                "dbo.Posts",
+                c => new
+                    {
+                        PostId = c.Int(nullable: false, identity: true),
+                        Title = c.String(),
+                        Content = c.String(),
+                        BlogId = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.PostId)
+                .ForeignKey("dbo.Blogs", t => t.BlogId, cascadeDelete: true)
+                .Index(t => t.BlogId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropIndex("dbo.Posts", new[] { "BlogId" });
+            DropForeignKey("dbo.Posts", "BlogId", "dbo.Blogs");
+            DropTable("dbo.Posts");
+            DropTable("dbo.Blogs");
+        }
+    }
+}
--- StartingWithEntityFramework/CodeFirstWithNewDatabase/Program.cs
+++ StartingWithEntityFramework/CodeFirstWithNewDatabase/Program.cs
@@ -44,6 +44,7 @@ public class Blog
 {
     public int BlogId { get; set; }
     public string Name { get; set; }
+    public string Url { get; set; }
 
     public virtual List<Post> Posts { get; set; }
 }
